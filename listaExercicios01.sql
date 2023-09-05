19797f1ae3f3f1e9b0c8ad04475ed5fbd6f847bf
Gabriel Pacheco <gabrielenaty0923@gmail.com>
2023-09-04 23:56:46 -0300

    exercicio dezenove

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -188,3 +188,9 @@ FROM vendas
 GROUP BY produto;
 
 -- exercício 18 CANCELADO
+
+-- exercício 19
+SELECT alunos.nome, COUNT(matriculas.id) AS numero_de_matriculas
+FROM alunos
+LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
+GROUP BY alunos.id;
