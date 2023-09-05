71c92cce347b2671945b9ce6a7d81236c470e99c
Gabriel Pacheco <gabrielenaty0923@gmail.com>
2023-09-04 23:30:03 -0300

    exercicio tres

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -101,4 +101,9 @@ select titulo from livros;
 -- exercício 2
 select NOME from autores
 WHERE nascimento < '1900-01-01';
-a
+
+-- exercício 3
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
